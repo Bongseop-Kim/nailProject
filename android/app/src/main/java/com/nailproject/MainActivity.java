8144288ce3219a3924f80b6061ba0994875830d6
Bongseop-Kim <xbs0427@naver.com>
2023-08-22 14:50:27 +0900

    android setting 완료 카카오톡 로그인

--- android/app/src/main/java/com/nailproject/MainActivity.java
+++ android/app/src/main/java/com/nailproject/MainActivity.java
@@ -5,22 +5,25 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.react.ReactActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   @Override
   protected String getMainComponentName() {
     return "nailProject";
   }
 
-  /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
-   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
@@ -29,4 +32,34 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
+
+  @Override
+  protected void onCreate(Bundle saveInstanceState) {
+    super.onCreate(saveInstanceState);
+    // setContentView(R.layout.activity_main);
+
+    getHashKey();
+  }
+
+  private void getHashKey() {
+    PackageInfo packageInfo = null;
+    try {
+      packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+    if (packageInfo == null)
+      Log.e("KeyHash", "KeyHash:null");
+
+    for (Signature signature : packageInfo.signatures) {
+      try {
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+        Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+      } catch (NoSuchAlgorithmException e) {
+        Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+      }
+    }
+  }
+
 }
--- screens/UserMenuScreen.jsx
+++ screens/UserMenuScreen.jsx
@@ -1,18 +1,44 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import MenuItem from '../components/MenuItem';
+import { getProfile, login } from '@react-native-seoul/kakao-login';
 
 const UserMenuScreen = () => {
   const navigation = useNavigation();
 
   const onLogin = () => navigation.navigate('Login');
   const onRegister = () => navigation.navigate('Register');
-  const onKakao = () => navigation.navigate('Kakao');
+
+  const signInWithKakao = async () => {
+    const token = await login();
+
+    console.log(JSON.stringify(token));
+  };
+
+  const signOutWithKakao = async () => {
+    const message = await logout();
+
+    setResult(message);
+  };
+
+  const getKakaoProfile = async () => {
+    const profile = await getProfile();
+
+    console.log(profile);
+  };
+
+  const unlinkKakao = async () => {
+    const message = await unlink();
+
+    setResult(message);
+  };
+
   return (
     <>
       <MenuItem name="로그인" onPress={onLogin} />
-      <MenuItem name="카카오로그인" onPress={onKakao} />
+      <MenuItem name="카카오로그인" onPress={signInWithKakao} />
       <MenuItem name="회원가입" onPress={onRegister} />
+      <MenuItem name="profile" onPress={getKakaoProfile} />
     </>
   );
 };
